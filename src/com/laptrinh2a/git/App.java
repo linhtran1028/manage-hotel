28a1e18a205e2a5965b5e093502f8eb9873ac910
linhtran1028 <trankhanhlinh.1028@gmail.com>
2022-03-16 14:26:00 +0700

    Test

--- src/com/laptrinh2a/git/App.java
+++ src/com/laptrinh2a/git/App.java
@@ -3,6 +3,6 @@ package com.laptrinh2a.git;
 public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Ahh Linh nè" );
+        System.out.println( "1233" );
     }
 }
