6f3a36a11e911dba79902424f1c2769cb7b62f82
linhtran1028 <trankhanhlinh.1028@gmail.com>
2022-03-16 13:43:59 +0700

    code moi neee

--- src/com/laptrinh2a/git/App.java
+++ src/com/laptrinh2a/git/App.java
@@ -3,6 +3,6 @@ package com.laptrinh2a.git;
 public class App {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!23123eqw" );
+        System.out.println( "Ahh Linh nè" );
     }
 }
